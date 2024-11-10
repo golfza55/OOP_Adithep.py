3b23e91deb49834cad3aa97fe7d14d714ad57f54
Adithep_tnk <157878553+golfza55@users.noreply.github.com>
2024-11-10 19:17:04 +0700

    kk

---
+++ test.py
@@ -0,0 +1,13 @@
+from time import sleep
+i = 0
+while True:
+     i += 1
+     print(f"รอบที่ {i}")
+     print ("hello python")
+     print("name Adithep chonlasit")
+     print("nickname Golf")
+     print("age 18")
+     print("number 21")
+     print("Student ID 67319010021")
+     print("hobby sing a sog")
+     sleep(5)
